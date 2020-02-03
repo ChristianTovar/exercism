4141691177686c7442f6292bc310d97f08551328
Christian Tovar <christian@kommit.co>
2020-02-03 17:20:26 -0500

    finished acronym exercise

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -7,6 +7,7 @@ defmodule Acronym do
   def abbreviate(string) do
     string
     |> String.replace(~r"[^a-zA-Z0-9' ]", " ")
+    |> String.replace(~r/([a-z])([A-Z])/, "\\1 \\2")
     |> String.split(" ")
     |> Enum.map(&String.at(&1, 0))
     |> Enum.join()
---
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,10 @@
+defmodule RomanNumerals do
+  @doc """
+  Convert the number to a roman number.
+  """
+  @numeral ~w(I II III IV V VI VII VIII XI X)
+
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+  end
+end
