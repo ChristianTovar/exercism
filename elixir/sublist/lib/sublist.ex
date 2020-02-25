540cc11ab645f37796557213af162caa42582306
ChristianTovar <tovarchristian21@gmail.com>
2020-02-24 20:26:11 -0500

    added naive solution

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -8,22 +8,20 @@ defmodule Sublist do
   def compare(_a, []), do: :superlist
 
   def compare(a, b) do
+    size_a = length(a)
+    size_b = length(b)
+
     cond do
-      length(a) < length(b) or length(a) == length(b) -> is_inside?(a, b, :sublist)
-      length(a) > length(b) -> is_inside?(b, a, :superlist)
+      size_a < size_b or size_a == size_b -> is_inside?(a, b, :sublist, size_a - 1, 0)
+      size_a > size_b -> is_inside?(b, a, :superlist, size_b - 1, 0)
     end
   end
 
-  defp is_inside?([ha | ta], [hb | tb], type) when ha === hb do
-    is_inside?(ta, tb, type)
+  defp is_inside?(a, b, type, size, acc) do
+    case Enum.slice(b, acc..(size + acc)) do
+      [] -> :unequal
+      ^a -> type
+      _different -> is_inside?(a, b, type, size, acc + 1)
+    end
   end
-
-  defp is_inside?([ha | _ta] = a, [hb | tb], type) when ha !== hb do
-    is_inside?(a, tb, type)
-  end
-
-  defp is_inside?([ha | _ta], [hb | _tb], _type) when ha !== hb, do: :unequal
-  defp is_inside?([], [], type), do: type
-  defp is_inside?([], [_hn | _tb], type), do: type
-  defp is_inside?([_ha | _ta], [], _type), do: :unequal
 end
