b7042595d123d2b28a53b1f4d876062676be94df
ChristianTovar <tovarchristian21@gmail.com>
2020-02-27 16:32:51 -0500

    added matching brackets solution

---
+++ elixir/matching-brackets/lib/matching_brackets.ex
@@ -0,0 +1,38 @@
+defmodule MatchingBrackets do
+  @doc """
+  Checks that all the brackets and braces in the string are matched correctly, and nested correctly
+  """
+  @spec check_brackets(String.t()) :: boolean
+  def check_brackets(""), do: true
+
+  def check_brackets(string) do
+    string
+    |> String.replace(~r/[^(){}\[\]]/, "")
+    |> String.graphemes()
+    |> Enum.reduce([], fn value, acc -> stack(value, acc) end)
+    |> is_balanced?()
+  end
+
+  defp stack(value, acc) when value in ["(", "[", ""] do
+    [value | acc]
+  end
+
+  defp stack(value, [head | tail]) when value == "]" and head == "[" do
+    tail
+  end
+
+  defp stack(value, [head | tail]) when value == "}" and head == "{" do
+    tail
+  end
+
+  defp stack(value, [head | tail]) when value == ")" and head == "(" do
+    tail
+  end
+
+  defp stack(value, acc) do
+    [value | acc]
+  end
+
+  defp is_balanced?([]), do: true
+  defp is_balanced?(_), do: false
+end
