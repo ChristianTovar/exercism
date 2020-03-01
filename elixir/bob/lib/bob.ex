0652e8208b5bcfdd530a87b2957adf1fdbd41e5a
ChristianTovar <tovarchristian21@gmail.com>
2020-03-01 15:53:54 -0500

    added Bob solution

---
+++ elixir/bob/lib/bob.ex
@@ -0,0 +1,20 @@
+defmodule Bob do
+  def hey(input) do
+    cond do
+      silence?(input) -> "Fine. Be that way!"
+      asking?(input) and shouting?(input) -> "Calm down, I know what I'm doing!"
+      asking?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
+  end
+
+  defp silence?(input), do: Regex.match?(~r/^\s*$/, input)
+
+  defp asking?(input), do: Regex.match?(~r/^.+\?\s*$/, input)
+
+  defp shouting?(input) do
+    formated_input = String.replace(input, ~r/[^a-zA-Z]/, "")
+    Regex.match?(~r/^[A-Z]+$/, formated_input)
+  end
+end
