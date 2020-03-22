08965384610ef9eedc5c3d6b4295027e956c9249
ChristianTovar <tovarchristian21@gmail.com>
2020-03-22 13:35:51 -0500

    added rows solution

---
+++ elixir/matrix/lib/matrix.ex
@@ -0,0 +1,58 @@
+defmodule Matrix do
+  defstruct matrix: nil
+
+  @doc """
+  Convert an `input` string, with rows separated by newlines and values
+  separated by single spaces, into a `Matrix` struct.
+  """
+  @spec from_string(input :: String.t()) :: %Matrix{}
+  def from_string(input), do: String.split(input, "\n")
+
+  @doc """
+  Write the `matrix` out as a string, with rows separated by newlines and
+  values separated by single spaces.
+  """
+  @spec to_string(matrix :: %Matrix{}) :: String.t()
+  def to_string(matrix), do: Enum.join(matrix, "\n")
+
+  @doc """
+  Given a `matrix`, return its rows as a list of lists of integers.
+  """
+  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
+  def rows(matrix) do
+    matrix
+    |> Enum.map(&to_integers/1)
+  end
+
+  defp to_integers(row) do
+    row
+    |> String.replace(" ", "")
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the row at `index`.
+  """
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def row(matrix, index) do
+    matrix
+    |> rows()
+    |> Enum.at(index)
+  end
+
+  @doc """
+  Given a `matrix`, return its columns as a list of lists of integers.
+  """
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
+  def columns(matrix) do
+    rows(matrix)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the column at `index`.
+  """
+  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def column(matrix, index) do
+  end
+end
