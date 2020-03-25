c9c6c8fc032facfd07b205427a3a229f28fe7a0c
ChristianTovar <tovarchristian21@gmail.com>
2020-03-24 19:46:35 -0500

    improved big-O

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -10,13 +10,17 @@ defmodule Hamming do
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(strand1, strand1), do: {:ok, 0}
 
-  def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
+  def hamming_distance(strand1, strand2) do
     get_difference(strand1, strand2, 0)
   end
 
-  def hamming_distance(_, _), do: {:error, "Lists must be the same length"}
-
   defp get_difference([], [], acc), do: {:ok, acc}
+
+  defp get_difference([_head_1 | _tail_1], [], _acc),
+    do: {:error, "Lists must be the same length"}
+
+  defp get_difference([], [_head_1 | _tail_1], _acc),
+    do: {:error, "Lists must be the same length"}
 
   defp get_difference([head_1 | tail_1], [head_2 | tail_2], acc) when head_1 != head_2,
     do: get_difference(tail_1, tail_2, acc + 1)
