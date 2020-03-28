5d5c1e44dc46398cc68cf63eb77c0080bdffda3d
ChristianTovar <tovarchristian21@gmail.com>
2020-03-28 11:36:13 -0500

    fixed styling issues

--- elixir/robot-simulator/lib/robot_simulator.ex
+++ elixir/robot-simulator/lib/robot_simulator.ex
@@ -9,8 +9,8 @@ defmodule RobotSimulator do
 
   @spec create(direction :: atom, position :: {integer, integer}) :: any
   def create(direction \\ :north, position \\ {0, 0}) do
-    with {true, :valid_direction} <- valid_direction?(direction),
-         {true, :valid_position} <- valid_position?(position) do
+    with {true, :valid_direction} <- check_direction(direction),
+         {true, :valid_position} <- check_position(position) do
       %RobotSimulator{direction: direction, position: position}
     else
       {false, :invalid_direction} -> {:error, "invalid direction"}
@@ -50,45 +50,45 @@ defmodule RobotSimulator do
   @spec position(robot :: any) :: {integer, integer}
   def position(%RobotSimulator{position: value}), do: value
 
-  defp valid_direction?(direction) when direction in @directions, do: {true, :valid_direction}
-  defp valid_direction?(_), do: {false, :invalid_direction}
+  defp check_direction(direction) when direction in @directions, do: {true, :valid_direction}
+  defp check_direction(_), do: {false, :invalid_direction}
 
-  defp valid_position?({x, y}) when is_integer(x) and is_integer(y), do: {true, :valid_position}
-  defp valid_position?(_), do: {false, :invalid_position}
+  defp check_position({x, y}) when is_integer(x) and is_integer(y), do: {true, :valid_position}
+  defp check_position(_), do: {false, :invalid_position}
 
-  defp execute_instruction("A", %RobotSimulator{direction: :north, position: {x, y}}),
-    do: %RobotSimulator{direction: :north, position: {x, y + 1}}
+  defp execute_instruction("A", %RobotSimulator{direction: :north, position: {x, y}} = robot),
+    do: %{robot | position: {x, y + 1}}
 
-  defp execute_instruction("L", %RobotSimulator{direction: :north, position: {x, y}}),
-    do: %RobotSimulator{direction: :west, position: {x, y}}
+  defp execute_instruction("A", %RobotSimulator{direction: :south, position: {x, y}} = robot),
+    do: %{robot | position: {x, y - 1}}
 
-  defp execute_instruction("R", %RobotSimulator{direction: :north, position: {x, y}}),
-    do: %RobotSimulator{direction: :east, position: {x, y}}
+  defp execute_instruction("A", %RobotSimulator{direction: :west, position: {x, y}} = robot),
+    do: %{robot | position: {x - 1, y}}
 
-  defp execute_instruction("A", %RobotSimulator{direction: :south, position: {x, y}}),
-    do: %RobotSimulator{direction: :south, position: {x, y - 1}}
+  defp execute_instruction("A", %RobotSimulator{direction: :east, position: {x, y}} = robot),
+    do: %{robot | position: {x + 1, y}}
 
-  defp execute_instruction("L", %RobotSimulator{direction: :south, position: {x, y}}),
-    do: %RobotSimulator{direction: :east, position: {x, y}}
+  defp execute_instruction("L", %RobotSimulator{direction: :north} = robot),
+    do: %{robot | direction: :west}
 
-  defp execute_instruction("R", %RobotSimulator{direction: :south, position: {x, y}}),
-    do: %RobotSimulator{direction: :west, position: {x, y}}
+  defp execute_instruction("L", %RobotSimulator{direction: :south} = robot),
+    do: %{robot | direction: :east}
 
-  defp execute_instruction("A", %RobotSimulator{direction: :west, position: {x, y}}),
-    do: %RobotSimulator{direction: :west, position: {x - 1, y}}
+  defp execute_instruction("L", %RobotSimulator{direction: :west} = robot),
+    do: %{robot | direction: :south}
 
-  defp execute_instruction("L", %RobotSimulator{direction: :west, position: {x, y}}),
-    do: %RobotSimulator{direction: :south, position: {x, y}}
+  defp execute_instruction("L", %RobotSimulator{direction: :east} = robot),
+    do: %{robot | direction: :north}
 
-  defp execute_instruction("R", %RobotSimulator{direction: :west, position: {x, y}}),
-    do: %RobotSimulator{direction: :north, position: {x, y}}
+  defp execute_instruction("R", %RobotSimulator{direction: :north} = robot),
+    do: %{robot | direction: :east}
 
-  defp execute_instruction("A", %RobotSimulator{direction: :east, position: {x, y}}),
-    do: %RobotSimulator{direction: :east, position: {x + 1, y}}
+  defp execute_instruction("R", %RobotSimulator{direction: :south} = robot),
+    do: %{robot | direction: :west}
 
-  defp execute_instruction("L", %RobotSimulator{direction: :east, position: {x, y}}),
-    do: %RobotSimulator{direction: :north, position: {x, y}}
+  defp execute_instruction("R", %RobotSimulator{direction: :west} = robot),
+    do: %{robot | direction: :north}
 
-  defp execute_instruction("R", %RobotSimulator{direction: :east, position: {x, y}}),
-    do: %RobotSimulator{direction: :south, position: {x, y}}
+  defp execute_instruction("R", %RobotSimulator{direction: :east} = robot),
+    do: %{robot | direction: :south}
 end
