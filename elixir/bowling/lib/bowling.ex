4edb8c30fbac6c8be9bc7e6e383fe040a450d07e
ChristianTovar <tovarchristian21@gmail.com>
2020-06-17 11:41:48 -0500

    added module attributes

--- elixir/bowling/lib/bowling.ex
+++ elixir/bowling/lib/bowling.ex
@@ -4,6 +4,9 @@ defmodule Bowling do
     the game
   """
 
+  @strike 10
+  @max_frames 10
+
   @spec start() :: any
   def start, do: []
 
@@ -14,22 +17,22 @@ defmodule Bowling do
   """
 
   @spec roll(any, integer) :: any | String.t()
-  def roll([[a, b] | _] = throws, _) when a + b < 10 and length(throws) == 10,
+  def roll([[a, b] | _] = throws, _) when a + b < @strike and length(throws) == @max_frames,
     do: {:error, "Cannot roll after game is over"}
 
-  def roll([[_a, _b], [10] | _] = throws, _) when length(throws) == 11,
+  def roll([[_a, _b], [@strike] | _] = throws, _) when length(throws) > @max_frames,
     do: {:error, "Cannot roll after game is over"}
 
-  def roll([[_], [a, b] | _] = throws, _) when a + b == 10 and length(throws) == 11,
+  def roll([[_], [a, b] | _] = throws, _) when a + b == @strike and length(throws) > @max_frames,
     do: {:error, "Cannot roll after game is over"}
 
   def roll(_, roll) when roll < 0, do: {:error, "Negative roll is invalid"}
-  def roll(_, roll) when roll > 10, do: {:error, "Pin count exceeds pins on the lane"}
+  def roll(_, roll) when roll > @strike, do: {:error, "Pin count exceeds pins on the lane"}
   def roll([], roll), do: [[roll]]
   def roll([[10] | _] = throws, roll), do: [[roll] | throws]
   def roll([[_throw_1, _throw_2] | _] = throws, roll), do: [[roll] | throws]
 
-  def roll([[throw_1] | _], roll) when throw_1 + roll > 10,
+  def roll([[throw_1] | _], roll) when throw_1 + roll > @strike,
     do: {:error, "Pin count exceeds pins on the lane"}
 
   def roll([[throw_1] | tail], roll), do: [[throw_1, roll] | tail]
@@ -40,7 +43,7 @@ defmodule Bowling do
   """
 
   @spec score(any) :: integer | String.t()
-  def score(throws) when length(throws) < 10,
+  def score(throws) when length(throws) < @max_frames,
     do: {:error, "Score cannot be taken until the end of the game"}
 
   def score(throws) do
@@ -49,35 +52,37 @@ defmodule Bowling do
     |> calculate_score(0, 1)
   end
 
-  defp calculate_score([[10]], _score, 10),
+  defp calculate_score([[@strike]], _score, @max_frames),
     do: {:error, "Score cannot be taken until the end of the game"}
 
-  defp calculate_score([[10], [10]], _score, 10),
+  defp calculate_score([[@strike], [@strike]], _score, @max_frames),
     do: {:error, "Score cannot be taken until the end of the game"}
 
-  defp calculate_score([[10], [a, b]], score, 10), do: score + 10 + a + b
-  defp calculate_score([[10], [a], [b]], score, 10), do: score + 10 + a + b
+  defp calculate_score([[@strike], [a, b]], score, @max_frames), do: score + @strike + a + b
+  defp calculate_score([[@strike], [a], [b]], score, @max_frames), do: score + @strike + a + b
 
-  defp calculate_score([[a, b]], _score, 10) when a + b == 10,
+  defp calculate_score([[a, b]], _score, @max_frames) when a + b == @strike,
     do: {:error, "Score cannot be taken until the end of the game"}
 
-  defp calculate_score([[a, b], [10]], score, 10) when a + b == 10, do: score + a + b + 10
+  defp calculate_score([[a, b], [@strike]], score, @max_frames) when a + b == @strike,
+    do: score + a + b + @strike
+
   defp calculate_score([[a]], score, _), do: score + a
   defp calculate_score([], score, _), do: score
 
-  defp calculate_score([[10] | [[a, b] | _] = tail], score, frame),
-    do: calculate_score(tail, score + 10 + a + b, frame + 1)
+  defp calculate_score([[@strike] | [[a, b] | _] = tail], score, frame),
+    do: calculate_score(tail, score + @strike + a + b, frame + 1)
 
-  defp calculate_score([[10] | [[10], [a, _b] | _] = tail], score, frame),
-    do: calculate_score(tail, score + 20 + a, frame + 1)
+  defp calculate_score([[@strike] | [[@strike], [a, _b] | _] = tail], score, frame),
+    do: calculate_score(tail, score + 2 * @strike + a, frame + 1)
 
-  defp calculate_score([[10] | [[10], [10] | _] = tail], score, frame),
-    do: calculate_score(tail, score + 30, frame + 1)
+  defp calculate_score([[@strike] | [[@strike], [@strike] | _] = tail], score, frame),
+    do: calculate_score(tail, score + 3 * @strike, frame + 1)
 
-  defp calculate_score([[a, b] | [[10] | _] = tail], score, frame) when a + b == 10,
-    do: calculate_score(tail, score + 20, frame + 1)
+  defp calculate_score([[a, b] | [[@strike] | _] = tail], score, frame) when a + b == @strike,
+    do: calculate_score(tail, score + 2 * @strike, frame + 1)
 
-  defp calculate_score([[a, b] | [[c, _d] | _] = tail], score, frame) when a + b == 10,
+  defp calculate_score([[a, b] | [[c, _d] | _] = tail], score, frame) when a + b == @strike,
     do: calculate_score(tail, score + a + b + c, frame + 1)
 
   defp calculate_score([[a, b] | tail], score, frame),
